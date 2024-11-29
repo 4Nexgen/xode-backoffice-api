691acbfe1518dcefea4fdc2a6626eb7cd92e9b82
butchcanz <butch.canada1994@gmail.com>
2024-11-29 17:23:12 +0800

    Upload file

---
+++ src/storage/storage.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  UseInterceptors,
+  UploadedFile,
+  Res,
+  Request,
+} from '@nestjs/common';
+import { StorageService } from './storage.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import * as fs from 'fs';
+import { Response } from 'express';
+import { Request as ExpressRequest } from 'express';
+
+@Controller('storage')
+export class StorageController {
+  constructor(private readonly storageService: StorageService) {}
+
+  @Post('uploadFile')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Request() request: ExpressRequest,
+  ) {
+    const baseUrl = `${request.protocol}://${request.get('host')}`;
+
+    const uploadFile = await this.storageService.uploadFile(file);
+
+    const fileUrl = `${baseUrl}/storage/${uploadFile.fileName}`;
+
+    return {
+      fileName: uploadFile.fileName,
+      fileUrl: fileUrl,
+      fileLocalPath: uploadFile.fileLocalPath,
+    };
+  }
+
+  @Get(':fileName')
+  getFile(@Param('fileName') fileName: string, @Res() res: Response) {
+    const { filePath, mimeType } = this.storageService.getFile(fileName);
+
+    res.setHeader('Content-Type', mimeType);
+    res.setHeader('Content-Disposition', `inline; filename="${fileName}"`);
+
+    const fileStream = fs.createReadStream(filePath);
+    fileStream.pipe(res);
+  }
+}
---
+++ src/storage/storage.service.ts
@@ -0,0 +1,77 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+
+@Injectable()
+export class StorageService {
+  private readonly STORAGE_PATH = path.join(
+    process.cwd(),
+    process.env.STORAGE_PATH,
+  );
+
+  private readonly fileTypeMap: Record<string, string> = {
+    '.jpg': 'images',
+    '.jpeg': 'images',
+    '.png': 'images',
+    '.pdf': 'documents',
+    default: 'others',
+  };
+
+  private readonly mediaTypesMap = {
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.png': 'image/png',
+    '.pdf': 'application/pdf',
+    default: 'application/octet-stream',
+  };
+
+  async uploadFile(
+    file: Express.Multer.File,
+  ): Promise<{ fileName: string; fileUrl: string; fileLocalPath: string }> {
+    if (!file) {
+      throw new BadRequestException('No file uploaded');
+    }
+
+    const tempFilePath = file.path;
+    const fileExtension = path.extname(file.originalname).toLowerCase();
+    const folderName =
+      this.fileTypeMap[fileExtension] || this.fileTypeMap['default'];
+    const storagePath = path.join(this.STORAGE_PATH, folderName);
+
+    if (!fs.existsSync(storagePath)) {
+      fs.mkdirSync(storagePath, { recursive: true });
+    }
+
+    const filePath = path.join(storagePath, file.originalname);
+
+    fs.renameSync(tempFilePath, filePath);
+
+    const fileUrl = `/storage/${folderName}/${file.originalname}`;
+
+    return {
+      fileName: file.originalname,
+      fileUrl: fileUrl,
+      fileLocalPath: filePath,
+    };
+  }
+
+  getFile(fileName: string): { filePath: string; mimeType: string } {
+    const fileExtension = path.extname(fileName).toLowerCase();
+    const folderName =
+      this.fileTypeMap[fileExtension] || this.fileTypeMap['default'];
+    const filePath = path.join(this.STORAGE_PATH, folderName, fileName);
+
+    if (!fs.existsSync(filePath)) {
+      throw new NotFoundException(`File ${fileName} not found`);
+    }
+
+    const mimeType =
+      this.mediaTypesMap[fileExtension] || this.mediaTypesMap['default'];
+
+    return { filePath, mimeType };
+  }
+}
